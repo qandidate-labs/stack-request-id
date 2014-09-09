712dd2f73f58df8b9c692b4f48793eb7a3e13346
Fritsjan Bakker <fritsjan@qandidate.com>
2014-09-09 16:11:32 +0200

    Add LICENSE

--- test/Qandidate/Stack/RequestId/MonologProcessorTest.php
+++ test/Qandidate/Stack/RequestId/MonologProcessorTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the qandidate/stack-request-id package.
+ *
+ * (c) Qandidate.com <opensource@qandidate.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Qandidate\Stack\RequestId;
 
 use Qandidate\Stack\TestCase;
