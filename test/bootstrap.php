2fcb278f5896568388ac2d8919b2e296fc3c06a0
Fritsjan Bakker <fritsjan@qandidate.com>
2014-09-09 16:27:20 +0200

    Enable Travis

---
+++ test/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
+    $loader = require_once $file;
+    $loader->add('Qandidate\\Stack', __DIR__);
+} else {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
