d2ee33425f30df8e0dacf3343604b9974668f7d6
othillo <othillo@othillo.nl>
2017-07-08 23:52:45 +0200

    add and use phpunit as a dev dependency

--- test/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (file_exists($file = __DIR__.'/../vendor/autoload.php')) {
-    $loader = require_once $file;
-    $loader->add('Qandidate\\Stack', __DIR__);
-} else {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
